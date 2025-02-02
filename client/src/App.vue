270a5f77b0ecc588915e1223709eab53197a46c6
maxim-dirtu <maxim.dirtu@gmail.com>
2025-02-02 16:45:50 +0200

    Vue project initialization

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Grocery List Manager</h1>
+    <a href="/about">About</a>
+    <a href="/">Home</a>
+  </div>
+  <RouterView/>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <p>Double Count: {{ doubleCount }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useCounterStore } from '../stores/counter';
+
+const counterStore = useCounterStore();
+
+const count = computed(() => counterStore.count);
+const doubleCount = computed(() => counterStore.doubleCount);
+
+function increment() {
+  counterStore.increment();
+}
+</script>
